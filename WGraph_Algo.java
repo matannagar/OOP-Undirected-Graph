a238aa9fa9e77cb43e978021ad4bf0466b8a80b2
Matan Ben Nagar <Matannagar@gmail.com>
2020-11-08 20:39:41 +0200

    added implementations

---
+++ WGraph_Algo.java
@@ -0,0 +1,55 @@
+package ex1;
+
+import java.util.List;
+
+public class WGraph_Algo implements weighted_graph_algorithms{
+
+	@Override
+	public void init(weighted_graph g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public weighted_graph getGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public weighted_graph copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double shortestPathDist(int src, int dest) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<node_info> shortestPath(int src, int dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(String file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean load(String file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ WGraph_DS.java
@@ -0,0 +1,79 @@
+package ex1;
+
+import java.util.Collection;
+
+public class WGraph_DS implements weighted_graph{
+
+	@Override
+	public node_info getNode(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasEdge(int node1, int node2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getEdge(int node1, int node2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addNode(int key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void connect(int node1, int node2, double w) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<node_info> getV() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<node_info> getV(int node_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public node_info removeNode(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeEdge(int node1, int node2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int nodeSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int edgeSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
